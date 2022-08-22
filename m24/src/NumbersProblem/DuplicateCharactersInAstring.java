d06227ae8eab744899a71e8d7bbbba2e57946b71
Arbaz <Arbaz@DESKTOP-KNU8G7H>
2022-08-22 15:48:53 +0530

    edit

--- m24/src/NumbersProblem/DuplicateCharactersInAstring.java
+++ m24/src/NumbersProblem/DuplicateCharactersInAstring.java
@@ -6,6 +6,7 @@ public class DuplicateCharactersInAstring {
 
 	public static void main(String[] args) {
 		String original;
+		
 		int count ;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter The String  ");
--- m24/src/NumbersProblem/FactorialOfANumber.java
+++ m24/src/NumbersProblem/FactorialOfANumber.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 public class FactorialOfANumber {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter The Numebers  ");
-		int num = sc.nextInt();
-		if (num < 0)
-			System.out.print("Factorial is NOt Negative");
-		else if (num == 0)
-			System.out.print("factorial of 0 is 1");
-		else {
-			for (int i=1;i <= num; ++i) {
-				if (num % i ==0) {
-					System.out.print(i+" , ");
-				}
-				}
-				}
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter The Numebers  ");
+			int num = sc.nextInt();
+			int fact =1;
+			if (num < 0)
+				System.out.print("Factorial is NOt Negative");
+			else if (num == 0)
+				System.out.print("factorial of 0 is 1");
+			else {
+				for (int i=1;i <= num; ++i) {
+					fact=fact*i;
+					//if (num % i ==0) 
+					{
+						System.out.print(i+" , ");
+					}
+					}
+				System.out.print("\n Factorial of the number: "+fact);
+					}
+		}
 		}
 
 }
