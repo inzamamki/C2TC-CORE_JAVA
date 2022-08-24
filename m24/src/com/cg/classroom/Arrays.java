219fae339820e7224dd850e043843d9e35457617
Arbaz <Arbaz@DESKTOP-KNU8G7H>
2022-08-24 16:41:22 +0530

    arrays

---
+++ m24/src/com/cg/classroom/Arrays.java
@@ -0,0 +1,24 @@
+package com.cg.classroom;
+
+import java.util.Scanner;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int size ;
+		System.out.print("Enter The Size of Arrays  ");
+		Scanner s=new Scanner(System.in);
+		size = s.nextInt();
+		int[] arr = new int [10];
+		System.out.print("Enter The element of the Arrays  ");
+		for (int i = 0; i<size;i++)
+		{
+			arr[i]=s.nextInt();
+		}{
+			for (int i =0; i<size;i++)
+			{System.out.print(arr[i]);
+		}
+
+	}
+
+}}
---
+++ m24/src/com/cg/classroom/TwoDimensional.java
@@ -0,0 +1,11 @@
+package com.cg.classroom;
+
+public class TwoDimensional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] x = new int [2][3];
+		System.out.println(x[0][1]);
+	}
+
+}
