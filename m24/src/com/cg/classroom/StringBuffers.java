24cd50b523ae109d6d3d92a511ef4d473c219fd3
Arbaz <Arbaz@DESKTOP-KNU8G7H>
2022-08-24 17:56:49 +0530

    string

---
+++ m24/src/com/cg/classroom/StringBuffers.java
@@ -0,0 +1,10 @@
+package com.cg.classroom;
+
+public class StringBuffers {
+	public static void main(String[] args) {
+		StringBuffer s = new StringBuffer("Avantika");
+	    s.append("Bisht");
+		System.out.println(s);
+
+	}
+}
---
+++ m24/src/com/cg/classroom/StringDemo.java
@@ -0,0 +1,12 @@
+package com.cg.classroom;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String s = new String ("Avantika");
+		s.concat ("Bisht");
+		System.out.println(s);
+
+	}
+
+}
