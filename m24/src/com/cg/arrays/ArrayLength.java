065ff51b350b951b4a247d28f099fe0b8c6f112f
Arbaz <Arbaz@DESKTOP-KNU8G7H>
2022-08-25 17:51:32 +0530

    Arraylength

---
+++ m24/src/com/cg/arrays/ArrayLength.java
@@ -0,0 +1,17 @@
+package com.cg.arrays;
+import java.util.Arrays;
+
+public class ArrayLength {
+
+		public static void main(String[] args) {
+	    int[] arr= {10,58,75,68,12,96,38,74,18,96}; //insert Array
+		
+		
+			{
+		
+			System.out.println("LENGTH OF THE ARRAY IS =");
+			System.out.println(arr.length); //number of characters in array
+		}
+		
+		}
+	}
